2ed66e401a5036fb7d4013a90ec7cac24393e24e
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2025-03-07 12:47:02 +0400

    refactor: update header & footer

--- template/js/Menu.js
+++ template/js/Menu.js
@@ -2,43 +2,52 @@ export default function initMobileMenu() {
     document.addEventListener("DOMContentLoaded", () => {
         const menuBtn = document.querySelector(".header__menu-btn");
         const closeBtn = document.querySelector(".header__menu-close");
-        const mobileMenu = document.getElementById("header-menu");
+        const mobileMenu = document.getElementById("menu-mobile");
         const overlay = document.getElementById("overlay");
         const body = document.body;
+        const menuLinks = mobileMenu.querySelectorAll("a"); // Все ссылки в меню
 
-        if (!menuBtn || !mobileMenu || !closeBtn || !overlay) return;
+        if (!menuBtn || !mobileMenu || !closeBtn || !overlay || !menuLinks)
+            return;
+
+        // Закрыть меню
+        function closeMenu() {
+            mobileMenu.classList.remove("active");
+            overlay.classList.remove("active");
+            body.classList.remove("menu-open");
+        }
 
         // Открыть меню
         menuBtn.addEventListener("click", () => {
             mobileMenu.classList.add("active");
             overlay.classList.add("active");
             body.classList.add("menu-open");
-
-            setTimeout(() => {
-                overlay.style.opacity = "1";
-            }, 10);
         });
 
         // Закрыть кнопкой
-        closeBtn.addEventListener("click", () => {
-            mobileMenu.classList.remove("active");
-            overlay.style.opacity = "0";
-
-            setTimeout(() => {
-                overlay.classList.remove("active");
-                body.classList.remove("menu-open");
-            }, 400);
-        });
+        closeBtn.addEventListener("click", closeMenu);
 
         // Закрыть оверлеем
-        overlay.addEventListener("click", () => {
-            mobileMenu.classList.remove("active");
-            overlay.style.opacity = "0";
+        overlay.addEventListener("click", closeMenu);
 
-            setTimeout(() => {
-                overlay.classList.remove("active");
-                body.classList.remove("menu-open");
-            }, 400);
+        // Закрыть кликом по ссылке
+        menuLinks.forEach((link) => {
+            link.addEventListener("click", (event) => {
+                // Проверить является ли якорной
+                if (link.getAttribute("href").startsWith("#")) {
+                    event.preventDefault(); // Отменить стандартный переход для плавного скролла
+                    const targetId = link.getAttribute("href");
+                    const targetElement = document.querySelector(targetId);
+
+                    if (targetElement) {
+                        window.scrollTo({
+                            top: targetElement.offsetTop,
+                            behavior: "smooth",
+                        });
+                    }
+                }
+                closeMenu();
+            });
         });
     });
 }
