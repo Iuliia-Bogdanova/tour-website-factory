8a48ca27ab0fb0489f27aa0c08444af87906b60c
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2025-03-13 18:53:39 +0400

    feat: animation add & correction

--- template/js/PlanetsAnimation.js
+++ template/js/PlanetsAnimation.js
@@ -2,6 +2,7 @@ export default function initPlanetsAnimation() {
     document.addEventListener("DOMContentLoaded", () => {
         const aboutSection = document.querySelector(".about");
         const whyAsSection = document.querySelector(".why-us");
+        const startSection = document.querySelector(".start");
         let lastScrollY = window.scrollY;
 
         window.addEventListener("scroll", () => {
@@ -13,12 +14,16 @@ export default function initPlanetsAnimation() {
                     aboutSection.classList.remove("scrolling-up");
                     whyAsSection.classList.add("scrolling-down");
                     whyAsSection.classList.remove("scrolling-up");
+                    startSection.classList.add("scrolling-down");
+                    startSection.classList.remove("scrolling-up");
                 } else {
                     // Скролл вверх
                     aboutSection.classList.add("scrolling-up");
                     aboutSection.classList.remove("scrolling-down");
                     whyAsSection.classList.add("scrolling-up");
                     whyAsSection.classList.remove("scrolling-down");
+                    startSection.classList.add("scrolling-up");
+                    startSection.classList.remove("scrolling-down");
                 }
                 lastScrollY = currentScrollY;
             }
--- template/js/main.js
+++ template/js/main.js
@@ -5,26 +5,3 @@ import initPlanetsAnimation from "./PlanetsAnimation.js";
 initMobileMenu();
 initSpaceAnimation();
 initPlanetsAnimation();
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const aboutSection = document.querySelector(".about");
-//     let lastScrollY = window.scrollY;
-
-//     window.addEventListener("scroll", () => {
-//         if (window.innerWidth >= 640) {
-//             const currentScrollY = window.scrollY;
-//             if (currentScrollY > lastScrollY) {
-//                 // Скроллим вниз
-//                 aboutSection.classList.add("scrolling-down");
-//                 aboutSection.classList.remove("scrolling-up");
-//             } else {
-//                 // Скроллим вверх
-//                 aboutSection.classList.add("scrolling-up");
-//                 aboutSection.classList.remove("scrolling-down");
-//             }
-//             lastScrollY = currentScrollY;
-//         }
-//     });
-// });
-
-//
