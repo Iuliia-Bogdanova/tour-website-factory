4dc4de59e3044a9d40a4a5a9f9cd51245b300afb
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2025-03-13 20:42:09 +0400

    fix: add history section scrolling

--- template/js/PlanetsAnimation.js
+++ template/js/PlanetsAnimation.js
@@ -2,6 +2,9 @@ export default function initPlanetsAnimation() {
     document.addEventListener("DOMContentLoaded", () => {
         const aboutSection = document.querySelector(".about");
         const whyAsSection = document.querySelector(".why-us");
+        const historySection = document.querySelector(".history");
+        const historyWrapperSection =
+            document.querySelector(".history__wrapper");
         const startSection = document.querySelector(".start");
         let lastScrollY = window.scrollY;
 
@@ -14,6 +17,10 @@ export default function initPlanetsAnimation() {
                     aboutSection.classList.remove("scrolling-up");
                     whyAsSection.classList.add("scrolling-down");
                     whyAsSection.classList.remove("scrolling-up");
+                    historySection.classList.add("scrolling-down");
+                    historySection.classList.remove("scrolling-up");
+                    historyWrapperSection.classList.add("scrolling-down");
+                    historyWrapperSection.classList.remove("scrolling-up");
                     startSection.classList.add("scrolling-down");
                     startSection.classList.remove("scrolling-up");
                 } else {
@@ -22,6 +29,10 @@ export default function initPlanetsAnimation() {
                     aboutSection.classList.remove("scrolling-down");
                     whyAsSection.classList.add("scrolling-up");
                     whyAsSection.classList.remove("scrolling-down");
+                    historySection.classList.add("scrolling-up");
+                    historySection.classList.remove("scrolling-down");
+                    historyWrapperSection.classList.add("scrolling-up");
+                    historyWrapperSection.classList.remove("scrolling-down");
                     startSection.classList.add("scrolling-up");
                     startSection.classList.remove("scrolling-down");
                 }
